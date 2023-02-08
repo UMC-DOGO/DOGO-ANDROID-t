6280fd7352cae5fb2ac5ad3df1f6fadf7a8f11d5
Chaelii <100350128+Chaelii@users.noreply.github.com>
2023-02-08 16:25:32 +0900

    Revert "Feat/login, join"

--- app/src/main/java/com/umc/dogo/StartActivity.kt
+++ app/src/main/java/com/umc/dogo/StartActivity.kt
@@ -1,27 +1,13 @@
 package com.umc.dogo
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.umc.dogo.databinding.ActivitySetupBinding
-import com.umc.dogo.databinding.ActivityStartBinding
-import com.umc.dogo.sign.JoinActivity
-import com.umc.dogo.sign.LoginActivity
-import com.umc.dogo.sign.SetupActivity
 
 class StartActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityStartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityStartBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.button.setOnClickListener {
-            val intent = Intent(this, JoinActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+        setContentView(R.layout.activity_start)
 
 
     }
